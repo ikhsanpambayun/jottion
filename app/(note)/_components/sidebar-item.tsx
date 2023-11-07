76f7d496fab3fbd2324607f7bb0de432daa5d11b
Muhammad Ikhsan Asa Pambayun <ikhsan.pambayun@gmail.com>
2023-11-08 01:07:32 +0700

    change ctrl+k bg-color

--- app/(note)/_components/sidebar-item.tsx
+++ app/(note)/_components/sidebar-item.tsx
@@ -131,7 +131,7 @@ const SidebarItem = ({
 
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted dark:bg-primary/5 px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span>Ctrl</span> + k
         </kbd>
       )}
