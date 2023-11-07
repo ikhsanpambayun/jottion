f21ddada414fdc4faef0997062945a30df232bec
Muhammad Ikhsan Asa Pambayun <ikhsan.pambayun@gmail.com>
2023-11-08 00:54:41 +0700

    remove console.log

--- app/(note)/(routes)/documents/page.tsx
+++ app/(note)/(routes)/documents/page.tsx
@@ -28,8 +28,6 @@ const DocumentsPage = () => {
     });
   };
 
-  console.log(user?.username);
-
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
